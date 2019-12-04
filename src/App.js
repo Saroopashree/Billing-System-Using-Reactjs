b1c74dc221cef73eb528cd6e7168837dabef6395
Saroopashree <saroopa25@gmail.com>
2019-12-04 09:47:14 +0530

    Lift state of NewInvoice to App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,152 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import {
-  HashRouter
-} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Route } from "react-router";
+import {
+  checkboxState,
+  textInputState,
+  priceState
+} from "./utils/constants";
 
-import Root from "./components/root";
 import NewInvoice from "./components/newInvoice";
 import ViewParty from "./components/viewParty";
 
-function App() {
-  return (
-    <div className="App">
-      <HashRouter>
-        <Route path="/" exact component={NewInvoice} />
-        <Route path="/viewParty" component={ViewParty} />
-      </HashRouter>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      checkboxes: checkboxState,
+      key: "",
+      textInputs: {
+        billNumber: "",
+        billDate: "",
+        vehicleNumber: "",
+        weight: { ...textInputState },
+        rate: { ...textInputState },
+        partyAddress: "",
+        partyGSTIN: "",
+        cgst: "",
+        sgst: ""
+      },
+      price: { ...priceState }
+    };
+  }
+
+  handleCheckboxChange = event => {
+    const { name } = event.target;
+
+    this.setState(prevState => ({
+      checkboxes: {
+        ...prevState.checkboxes,
+        [name]: !prevState.checkboxes[name]
+      }
+    }));
+  };
+
+  handleTextInputChange = (event, label) => {
+    const { name, value } = event.target;
+
+    if (name === "weight") {
+      this.setState(
+        prevState => ({
+          ...prevState,
+          textInputs: {
+            ...prevState.textInputs,
+            ["weight"]: {
+              [label]: value
+            }
+          },
+          price: {
+            ...prevState.price,
+            [label]: (
+              Number(value) * Number(this.state.textInputs.rate[label])
+            ).toFixed(2)
+          }
+        })
+        // () => console.log("new price: ", this.state.price)
+      );
+    } else if (name === "rate") {
+      this.setState(
+        prevState => ({
+          ...prevState,
+          textInputs: {
+            ...prevState.textInputs,
+            ["rate"]: {
+              [label]: value
+            }
+          },
+          price: {
+            ...prevState.price,
+            [label]: (
+              Number(this.state.textInputs.weight[label]) * Number(value)
+            ).toFixed(2)
+          }
+        })
+        // () => console.log("new price: ", this.state.price)
+      );
+    } else {
+      this.setState(prevState => ({
+        textInputs: {
+          ...prevState.textInputs,
+          [name]: value
+        }
+      }));
+    }
+  };
+
+  handleKeyChange = event => {
+    const { value } = event.target;
+    const data = require("./assets/data.json");
+
+    const val = data["parties"][value.toString()];
+    this.setState({ key: value });
+
+    if (value && value <= Object.keys(data["parties"]).length) {
+      this.setState(prevState => ({
+        textInputs: {
+          ...prevState.textInputs,
+          partyAddress: val["Address"],
+          partyGSTIN: val["GSTIN"]
+        }
+      }));
+    } else {
+      this.setState(prevState => ({
+        textInputs: {
+          ...prevState.textInputs,
+          partyAddress: "",
+          partyGSTIN: ""
+        }
+      }));
+    }
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    alert("You are submitting ");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <HashRouter>
+          <Route
+            path="/"
+            exact
+            render={
+              (props) => <NewInvoice
+                {...props}
+                states={this.state}
+                handleKeyChange={this.handleKeyChange}
+                handleTextInputChange={this.handleTextInputChange}
+                handleCheckboxChange={this.handleCheckboxChange}
+                onSubmit={this.handleSubmit}
+              />}
+          />
+          <Route path="/viewParty" render={ViewParty} />
+        </HashRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
